7ef5c3fb50036511ae7b719b721d60e71196d2b7
kouma1990 <kouma1990@gmail.com>
2017-06-18 21:44:38 +0900

    プロジェクト ファイルを追加します。

---
+++ SQLConnector/SQLConnector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace SQLConnector
+{
+    public class SQLConnector : ContentPage
+    {
+        public SQLConnector()
+        {
+            var button = new Button
+            {
+                Text = "Click Me!",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            int clicked = 0;
+            button.Clicked += (s, e) => button.Text = "Clicked: " + clicked++;
+
+            Content = button;
+        }
+    }
+}
